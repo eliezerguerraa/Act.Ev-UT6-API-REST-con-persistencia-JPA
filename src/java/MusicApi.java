bdc5218cd5d435401bcc27f74a3e1e9272568343
eliezerguerraa <eliezergrsr07@gmail.com>
2026-06-06 16:51:16 +0100

    musicapi hecho

--- src/java/MusicApi.java
+++ src/java/MusicApi.java
@@ -1,7 +1,11 @@
 package java;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class MusicApi {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        SpringApplication.run(MusicApi.class, args);
     }
 }
